4c5269b78ef3c180e9411e7f6077db4676849c8d
youtao.wan <youtao.wan@qunar.com>
2017-08-11 17:18:20 +0800

    edit

---
+++ web-service/src/main/java/com/mohress/training/service/audit/Test.java
@@ -0,0 +1,13 @@
+package com.mohress.training.service.audit;
+
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Created by youtao.wan on 2017/8/11.
+ */
+public class Test {
+    AuthenticationFailureHandler test = null;
+
+    UsernamePasswordAuthenticationFilter filter = null;
+}
