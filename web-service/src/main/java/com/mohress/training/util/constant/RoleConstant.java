8fa21ea9091da4b25794e6d465327ddb3b3522f6
youtao.wan <youtao.wan@qunar.com>
2017-08-29 15:44:16 +0800

    角色常量Id

---
+++ web-service/src/main/java/com/mohress/training/util/constant/RoleConstant.java
@@ -0,0 +1,23 @@
+package com.mohress.training.util.constant;
+
+/**
+ * 角色常量
+ *
+ */
+public class RoleConstant {
+
+    /**
+     * root角色Id
+     */
+    public static final String ROOT_ROLE_ID = "17081610225621055995";
+
+    /**
+     * 人社局角色Id
+     */
+    public static final String PEOPLE_SOCIAL_ROLE_ID = "17081610225621055996";
+
+    /**
+     * 培训机构Id
+     */
+    public static final String AGENCY_ROLE_ID = "17081610225621055997";
+}
